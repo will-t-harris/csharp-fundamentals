d7b8a4ce93c294ccc1c2eadfc402b8fff7b0aee1
Will Harris <will.t.harris@protonmail.com>
2019-10-16 13:45:06 -0600

    declare two double variables and write the sum to console

--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -6,6 +6,11 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
+            double x = 34.1;
+            double y = 42.42;
+
+            Console.WriteLine(x + y);
+
             if (args.Length > 0)
             {
                 Console.WriteLine($"Hello {args[0]}!");
