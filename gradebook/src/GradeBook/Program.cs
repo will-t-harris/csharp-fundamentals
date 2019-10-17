742f359a3445f6e76bbbf00a271f6abdb46fed28
Will Harris <will.t.harris@protonmail.com>
2019-10-17 11:02:46 -0600

    calculate average of values in grades list using list.count

--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -8,17 +8,19 @@ namespace GradeBook
         static void Main(string[] args)
         {
             var numbers = new[] {4.222, 42.22, 422.2, 2};
-            var grades = new List<double>();
+            var grades = new List<double>() {4.222, 42.22, 422.2, 2};
             grades.Add(42);
 
-            var result = 0.0;
+            var sum = 0.0;
 
-            foreach(double number in numbers) 
+            foreach(double grade in grades) 
             {
-                result += number;
+                sum += grade;
             }
 
-            System.Console.WriteLine(result);
+            var average = sum / grades.Count;
+
+            System.Console.WriteLine(average);
 
             if (args.Length > 0)
             {
