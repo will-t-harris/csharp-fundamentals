232d3e1cd047d465e7b5217abd0a4ef9fe4216c4
Will Harris <will.t.harris@protonmail.com>
2019-10-18 14:01:57 -0600

    compute highest and lowest grades in list

--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -10,20 +10,27 @@ namespace GradeBook
             var book = new Book("My Grade Book");
             book.AddGrade(90.2);
             book.AddGrade(75.5);
+            book.AddGrade(88.8);
 
-            var grades = new List<double>() { 4.222, 42.22, 422.2, 2 };
+            var grades = new List<double>() { 4.222, 42.22, 76.2, 2 };
             grades.Add(42);
 
             var sum = 0.0;
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
 
             foreach (double grade in grades)
             {
+                highGrade = Math.Max(grade, highGrade);
+                lowGrade = Math.Min(grade, lowGrade);
                 sum += grade;
             }
 
             var average = sum / grades.Count;
 
-            System.Console.WriteLine($"The average grade is {average:N1}");
+            Console.WriteLine($"The average grade is {average:N1}");
+            Console.WriteLine($"The highest grade is {highGrade:N2}");
+            Console.WriteLine($"The lowest grade is {lowGrade:N2}");
         }
-    } 
+    }
 }
