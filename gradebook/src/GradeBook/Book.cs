42fb2a3b50b80583e8711fcc639a876bc28aa0d9
Will Harris <will.t.harris@protonmail.com>
2019-10-18 14:14:26 -0600

    move grade logic into book class in ShowStatistics method

--- gradebook/src/GradeBook/Book.cs
+++ gradebook/src/GradeBook/Book.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace GradeBook
@@ -13,6 +14,22 @@ namespace GradeBook
         public void AddGrade(double grade)
         {
             this.grades.Add(grade);
+        }
+
+        public void ShowStatistics(){
+            var average = 0.0;
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+
+            foreach(var grade in grades) {
+                lowGrade = Math.Min(grade, lowGrade);
+                highGrade = Math.Max(grade, highGrade);
+                average += grade;
+            }
+            average /= grades.Count;
+            Console.WriteLine($"The lowest grade is {lowGrade:N2}");
+            Console.WriteLine($"The highest grade is {highGrade:N2}");
+            Console.WriteLine($"The average grade is {average:N2}");
         }
 
         private List<double> grades;
--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -12,25 +12,7 @@ namespace GradeBook
             book.AddGrade(75.5);
             book.AddGrade(88.8);
 
-            var grades = new List<double>() { 4.222, 42.22, 76.2, 2 };
-            grades.Add(42);
-
-            var sum = 0.0;
-            var highGrade = double.MinValue;
-            var lowGrade = double.MaxValue;
-
-            foreach (double grade in grades)
-            {
-                highGrade = Math.Max(grade, highGrade);
-                lowGrade = Math.Min(grade, lowGrade);
-                sum += grade;
-            }
-
-            var average = sum / grades.Count;
-
-            Console.WriteLine($"The average grade is {average:N1}");
-            Console.WriteLine($"The highest grade is {highGrade:N2}");
-            Console.WriteLine($"The lowest grade is {lowGrade:N2}");
+            book.ShowStatistics();
         }
     }
 }
