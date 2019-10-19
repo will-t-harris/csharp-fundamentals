1ec5ae4a1b38fa1334c8077d8cd66dc5ab0382ac
Will Harris <will.t.harris@protonmail.com>
2019-10-19 16:56:42 -0600

    tweak equal statement to prevent double precision errors

--- gradebook/test/GradeBook.Tests/BookTests.cs
+++ gradebook/test/GradeBook.Tests/BookTests.cs
@@ -1,23 +1,26 @@
 using System;
 using Xunit;
 
-namespace GradeBook.Tests {
-    public class BookTests {
+namespace GradeBook.Tests
+{
+    public class BookTests
+    {
         [Fact]
-        public void Test1 () {
+        public void Test1()
+        {
             // arrange
-            var book = new Book ("");
-            book.AddGrade (89.1);
-            book.AddGrade (90.5);
-            book.AddGrade (77.7);
+            var book = new Book("");
+            book.AddGrade(89.1);
+            book.AddGrade(90.5);
+            book.AddGrade(77.7);
 
             // act
-            var result = book.GetStatistics ();
+            var result = book.GetStatistics();
 
             // assert
-            Assert.Equal (85.6, result.Average);
-            Assert.Equal (90.5, result.High);
-            Assert.Equal (77.3, result.Low);
+            Assert.Equal(85.8, result.Average, 1);
+            Assert.Equal(90.5, result.High, 1);
+            Assert.Equal(77.7, result.Low, 1);
         }
     }
 }
