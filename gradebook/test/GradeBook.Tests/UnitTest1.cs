18be17eb9f8963786a4358bfe9163d93d9eb94f8
Will Harris <will.t.harris@protonmail.com>
2019-10-19 14:49:32 -0600

    add failing case to Test1 to show test failure

--- gradebook/test/GradeBook.Tests/UnitTest1.cs
+++ gradebook/test/GradeBook.Tests/UnitTest1.cs
@@ -8,7 +8,13 @@ namespace GradeBook.Tests
         [Fact]
         public void Test1()
         {
+            var x = 5;
+            var y = 2;
 
+            var expected = 7;
+            var actual = x * y;
+
+            Assert.Equal(expected, actual);
         }
     }
 }
