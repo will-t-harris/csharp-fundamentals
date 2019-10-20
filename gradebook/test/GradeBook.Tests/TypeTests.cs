989d00facfd0da31a59b3cef7c01a801c92b4826
Will Harris <will.t.harris@protonmail.com>
2019-10-20 13:29:46 -0600

    add example using the out keyword to pass by ref, which requires that the output parameter is initialized

--- gradebook/test/GradeBook.Tests/TypeTests.cs
+++ gradebook/test/GradeBook.Tests/TypeTests.cs
@@ -10,12 +10,12 @@ namespace GradeBook.Tests
         public void CSharpCanPassByRef()
         {
             var book1 = GetBook("Book 1");
-            GetBookSetName(ref book1, "New Name");
+            GetBookSetName(out book1, "New Name");
 
             Assert.Equal("New Name", book1.Name);
         }
 
-        private void GetBookSetName(ref Book book, string name)
+        private void GetBookSetName(out Book book, string name)
         {
             book = new Book(name);
         }
