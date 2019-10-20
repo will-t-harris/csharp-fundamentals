10c1960ea6e6993553d506137b420d986b901218
Will Harris <will.t.harris@protonmail.com>
2019-10-20 13:22:36 -0600

    define test for whether csharp passes by value (it does)

--- gradebook/test/GradeBook.Tests/TypeTests.cs
+++ gradebook/test/GradeBook.Tests/TypeTests.cs
@@ -5,6 +5,21 @@ namespace GradeBook.Tests
 {
     public class TypeTests
     {
+
+        [Fact]
+        public void CSharpIsPassByValue()
+        {
+            var book1 = GetBook("Book 1");
+            GetBookSetName(book1, "New Name");
+
+            Assert.Equal("Book 1", book1.Name);
+        }
+
+        private void GetBookSetName(Book book, string name)
+        {
+            book = new Book(name);
+        }
+
         [Fact]
         public void CanSetNameFromReference()
         {
