966bb3e0d23a49546b9f2b573db04f30a8c97e76
Will Harris <will.t.harris@protonmail.com>
2019-10-22 18:57:41 -0600

    try to parse user input if a valid double entered

--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -8,9 +8,24 @@ namespace GradeBook
         static void Main(string[] args)
         {
             var book = new Book("My Grade Book");
+            
+            // use console.readline to get user input "please enter a grade"
+            do
+            {
+                Console.WriteLine("Please enter a grade (press the q key to exit)");      
+                var input = Console.ReadLine();
+                var isDouble = Double.TryParse(input, out double result);
+                if (isDouble)
+                {
+                    var grade = Double.Parse(input);
+                    book.AddGrade(grade);
+                }
+            } while (input.ToLower() != 'q');
+            // if the user enters a number, add the number to the gradebook
+            // if the user enters 'q', quit the loop
+            
             book.AddGrade(90.2);
-            book.AddGrade(75.5);
-            book.AddGrade(88.8);
+
 
             var stats = book.GetStatistics();
             Console.WriteLine($"The lowest grade is {stats.Low:N2}");
