62edd5d3558c66f2d5b751d4c5108b3a135dbed5
Will Harris <will.t.harris@protonmail.com>
2019-10-29 13:47:24 -0600

    refactor(book): use more succinct syntax

--- gradebook/src/GradeBook/Book.cs
+++ gradebook/src/GradeBook/Book.cs
@@ -58,11 +58,15 @@ namespace GradeBook
         public override void AddGrade(double grade)
         {
             // create stream writer
-            var writer = File.AppendText($"C:\\Users\\wharris\\Documents\\{Name}.txt");
-            // call write line to add grade to file
-            writer.WriteLine(grade);
-            // dispose of writer
-            writer.Dispose();
+            using(var writer = File.AppendText($"C:\\Users\\wharris\\Documents\\{Name}.txt"))
+            {
+                // call write line to add grade to file
+                writer.WriteLine(grade);
+                if(GradeAdded != null)
+                {
+                    GradeAdded(this, new EventArgs());
+                }
+            }
         }
 
         public override Statistics GetStatistics()
