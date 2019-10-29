2194284f0452ee3820cdfc54aeb334010f5ed0b6
Will Harris <will.t.harris@protonmail.com>
2019-10-29 11:25:59 -0600

    extract entergrades method in program.cs

--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -15,6 +15,18 @@ namespace GradeBook
             // if the user enters a number, add the number to the gradebook
             // if the user enters 'q', quit the loop
 
+            EnterGrades(book);
+
+            var stats = book.GetStatistics();
+            Console.WriteLine($"For the book named {book.Name}");
+            Console.WriteLine($"The lowest grade is {stats.Low:N2}");
+            Console.WriteLine($"The highest grade is {stats.High:N2}");
+            Console.WriteLine($"The average grade is {stats.Average:N2}");
+            Console.WriteLine($"The letter grade is {stats.Letter}");
+        }
+
+        private static void EnterGrades(InMemoryBook book)
+        {
             while (true)
             {
                 Console.WriteLine("Enter a grade or 'q' to quit");
@@ -30,11 +42,11 @@ namespace GradeBook
                     double grade = double.Parse(input);
                     book.AddGrade(grade);
                 }
-                catch(ArgumentException ex)
+                catch (ArgumentException ex)
                 {
                     Console.WriteLine(ex.Message);
                 }
-                catch(FormatException ex)
+                catch (FormatException ex)
                 {
                     Console.WriteLine(ex.Message);
                 }
@@ -43,13 +55,6 @@ namespace GradeBook
                     Console.WriteLine("**");
                 }
             }
-
-            var stats = book.GetStatistics();
-            Console.WriteLine($"For the book named {book.Name}");
-            Console.WriteLine($"The lowest grade is {stats.Low:N2}");
-            Console.WriteLine($"The highest grade is {stats.High:N2}");
-            Console.WriteLine($"The average grade is {stats.Average:N2}");
-            Console.WriteLine($"The letter grade is {stats.Letter}");
         }
 
         static void OnGradeAdded(object sender, EventArgs e)
